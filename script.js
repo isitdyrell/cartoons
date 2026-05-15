9f87f6f216289c5e4b8637dcb167714fb65a0015
isitdyrell <djamesrushlow@outlook.com>
2026-05-15 13:01:20 -0700

    update

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 const games = [
             {
                 title: "Lucky Stars",
-                image: "assets/activations/luckystars.png",
+                image: "assets/activations/luckystars.gif",
                 tags: ["Daily Spin", ],
                 description: "Spin once per day for a chance to win free NFTs, ETH, and exclusive rewards. Only verified Cartoons holders can play.",
                 buttons: [
