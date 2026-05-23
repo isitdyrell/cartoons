66b71076ce5a94be406d1d33bfea0a74b1804ba3
isitdyrell <djamesrushlow@outlook.com>
2026-05-22 17:21:48 -0700

    update

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ document.addEventListener('DOMContentLoaded', () => {
             },
             {
                 title: "Solana Wallet Collection",
-                image: "assets/activations/tooncoin.png",
+                image: "assets/activations/solanaexp.png",
                 tags: ["WL Registration"],
                 description: "We have an upcoming expansion project on SOLANA. Cartoons NFT holders can submit their wallets for early access.",
                 buttons: [
